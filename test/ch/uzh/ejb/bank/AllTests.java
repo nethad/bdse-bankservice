9fcc9250ce16b36b402db8c6190412d03ac0779d
Daniel Spicar <donnervogelistaken@gmail.com>
2011-05-20 15:23:38 +0200

    Added Test Suite to run all tests in one go.

---
+++ test/ch/uzh/ejb/bank/AllTests.java
@@ -0,0 +1,20 @@
+package ch.uzh.ejb.bank;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * For security to work, the following key value pair must be defined as part of
+ * the VM arguments: -Djava.security.auth.login.config=etc/login.config
+ * 
+ * @author daniel
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+	BankApplicationTest.class,
+	BankApplicationSecurityTest.class,
+	TestImports.class
+	})
+public class AllTests {}
